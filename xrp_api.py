7af4670dd41c4f59db59fecd1235589007e556f4
David Vilela <dvilelaf@gmail.com>
2019-10-08 20:49:22 +0200

    Add 500 internal server error to HttpResponses

--- xrp_api.py
+++ xrp_api.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 HttpResponses = {200: 'OK', 400: 'Bad request', 401: 'Unauthorized',
-                 403: 'Forbidden', 404: 'Not found'}
+                 403: 'Forbidden', 404: 'Not found', 500: 'Internal server error'}
 
 
 class XRPAPI():
