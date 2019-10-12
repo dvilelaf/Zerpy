ff1d7895b2a3cec53a73e52f6daf39bd8e5f0220
David Vilela <dvilelaf@gmail.com>
2019-10-12 21:27:58 +0200

    Include alias in sample config file

--- .secret_config-example.js
+++ .secret_config-example.js
@@ -4,6 +4,12 @@ module.exports = {
         "ACCOUNT_ADDRESS_HERE": {
             "apiKey": "RANDOM_STRING_HERE",
             "secret": "ACCOUNT_SECRET_HERE",
+            "alias": "ALIAS_HERE"
+        },
+        "ACCOUNT2_ADDRESS_HERE": {
+            "apiKey": "RANDOM_STRING_HERE",
+            "secret": "ACCOUNT2_SECRET_HERE",
+            "alias": "ALIAS2_HERE"
         }
     }
 }
